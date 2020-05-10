528f856ab1bc6bd8e48729a85dc6801dc01568c8
Nemat <nematengg@gmail.com>
2020-05-11 00:28:37 +0800

    setup espresso to test UI

--- app/src/androidTest/java/com/mobiledatatraffic/MainActivityTest.java
+++ app/src/androidTest/java/com/mobiledatatraffic/MainActivityTest.java
@@ -2,19 +2,50 @@ package com.mobiledatatraffic;
 
 import org.junit.Before;
 import org.junit.Rule;
-import androidx.test.rule.ActivityTestRule;
-import androidx.test.runner.AndroidJUnit4;
+import org.junit.Test;
 
-import static org.junit.Assert.*;
+import androidx.test.rule.ActivityTestRule;
+
+import okhttp3.mockwebserver.Dispatcher;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.RecordedRequest;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 
 public class MainActivityTest {
     @Rule
     public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
 
+    @Rule
+    public final MockServerRule mMockServerRule = new MockServerRule();
 
     @Before
     public void setUp() throws Exception {
+        mMockServerRule.server().setDispatcher(getDispatcher());
+        activityRule.launchActivity(null);
     }
 
+    @Test
+    public void seesDataVolumeList() {
+        onView(withText(R.string.header)).check(matches(isDisplayed()));
+    }
+
+    private Dispatcher getDispatcher() {
+        final Dispatcher dispatcher = new Dispatcher() {
+            @Override
+            public MockResponse dispatch(RecordedRequest request) throws InterruptedException {
+                if (request.getPath().equals("/name")){
+                    return new MockResponse().setResponseCode(200)
+                            .setBody("JazzJackTheRabbit");
+                }
+                throw new IllegalStateException("no mock set up for " + request.getPath());
+            }
+        };
+        return dispatcher;
+    }
 
 }
