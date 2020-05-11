9389166b0ab7a6db39568d681954abdd63ac92e1
Nemat <nemat@Nemats-MacBook-Pro.local>
2020-05-11 21:44:03 +0800

    Update espresso test

--- app/src/androidTest/java/com/mobiledatatraffic/MainActivityTest.java
+++ app/src/androidTest/java/com/mobiledatatraffic/MainActivityTest.java
@@ -1,51 +1,34 @@
 package com.mobiledatatraffic;
 
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-import androidx.test.rule.ActivityTestRule;
-
-import okhttp3.mockwebserver.Dispatcher;
-import okhttp3.mockwebserver.MockResponse;
-import okhttp3.mockwebserver.RecordedRequest;
-
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-
 
 public class MainActivityTest {
     @Rule
     public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
 
-    @Rule
-    public final MockServerRule mMockServerRule = new MockServerRule();
-
     @Before
     public void setUp() throws Exception {
-        mMockServerRule.server().setDispatcher(getDispatcher());
         activityRule.launchActivity(null);
+        InstrumentationRegistry.getInstrumentation().waitForIdleSync();
     }
 
     @Test
     public void seesDataVolumeList() {
-        onView(withText(R.string.header)).check(matches(isDisplayed()));
+        onView(withRecyclerView(R.id.data_list).atPosition(0))
+                .check(matches(hasDescendant(withText(R.string.header))));
     }
 
-    private Dispatcher getDispatcher() {
-        final Dispatcher dispatcher = new Dispatcher() {
-            @Override
-            public MockResponse dispatch(RecordedRequest request) throws InterruptedException {
-                if (request.getPath().equals("/name")){
-                    return new MockResponse().setResponseCode(200)
-                            .setBody("JazzJackTheRabbit");
-                }
-                throw new IllegalStateException("no mock set up for " + request.getPath());
-            }
-        };
-        return dispatcher;
+    private static RecyclerViewMatcher withRecyclerView(final int recyclerViewId) {
+        return new RecyclerViewMatcher(recyclerViewId);
     }
-
 }
