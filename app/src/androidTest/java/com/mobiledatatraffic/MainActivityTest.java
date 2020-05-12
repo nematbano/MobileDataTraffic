468cdd04c1fc6b84665eee31496d72ba0206e048
Nemat <nemat@Nemats-MacBook-Pro.local>
2020-05-12 08:09:25 +0800

    Add MainActivityTest

--- app/src/androidTest/java/com/mobiledatatraffic/MainActivityTest.java
+++ app/src/androidTest/java/com/mobiledatatraffic/MainActivityTest.java
@@ -7,14 +7,24 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.io.IOException;
+
+import okhttp3.mockwebserver.Dispatcher;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.RecordedRequest;
+import okio.Buffer;
+
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 public class MainActivityTest {
     @Rule
     public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+    @Rule
+    public final MockWebServerRule mMockServerRule = new MockWebServerRule();
 
     @Before
     public void setUp() throws Exception {
@@ -23,12 +33,38 @@ public class MainActivityTest {
     }
 
     @Test
-    public void seesDataVolumeList() {
+    public void onLoad_givenSuccess_seesDataVolumeList() {
+        mMockServerRule.server().setDispatcher(getDispatcher());
         onView(withRecyclerView(R.id.data_list).atPosition(0))
                 .check(matches(hasDescendant(withText(R.string.header))));
     }
 
+    @Test
+    public void onLoad_givenServerError_showsErrorPopup() {
+        mMockServerRule.server().enqueue(new MockResponse().setResponseCode(500));
+
+        onView(withText(R.string.error_message)).check(matches(isDisplayed()));
+    }
+
     private static RecyclerViewMatcher withRecyclerView(final int recyclerViewId) {
         return new RecyclerViewMatcher(recyclerViewId);
+    }
+
+    private Dispatcher getDispatcher(){
+        final Dispatcher dispatcher = new Dispatcher() {
+            @Override
+            public MockResponse dispatch(RecordedRequest request) throws InterruptedException {
+                if (request.getPath().startsWith("https://data.gov.sg/")) {
+                    try {
+                        return new MockResponse().setResponseCode(200)
+                                .setBody(new Buffer().readFrom(getClass().getClassLoader().getResourceAsStream("get.json")));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                throw new IllegalStateException("no mock set up for " + request.getPath());
+            }
+        };
+        return dispatcher;
     }
 }
---
+++ app/src/androidTest/java/com/mobiledatatraffic/MockWebServerRule.java
@@ -0,0 +1,58 @@
+package com.mobiledatatraffic;
+
+import androidx.test.rule.UiThreadTestRule;
+
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+import okhttp3.mockwebserver.MockWebServer;
+
+public class MockWebServerRule extends UiThreadTestRule {
+
+    private MockWebServer mServer;
+
+    public static final int MOCK_WEBSERVER_PORT = 8000;
+
+    @Override
+    public Statement apply(final Statement base, Description description) {
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                startServer();
+                try {
+                    base.evaluate();
+                } finally {
+                    stopServer();
+                }
+            }
+        };
+    }
+
+    /**
+     * Returns the started web server instance
+     *
+     * @return mock server
+     */
+    public MockWebServer server() {
+        return mServer;
+    }
+
+    public void startServer() throws IOException, NoSuchAlgorithmException {
+        mServer = new MockWebServer();
+        try {
+            mServer.start(MOCK_WEBSERVER_PORT);
+        } catch (IOException e) {
+            throw new IllegalStateException("mock server start issue", e);
+        }
+    }
+
+    public void stopServer() {
+        try {
+            mServer.shutdown();
+        } catch (IOException e) {
+        }
+    }
+}
